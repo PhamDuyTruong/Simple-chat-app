e992c4fbffb27062d1d9659ded5dc90a9d436a22
20120230 <duytruong5437@gmail.com>
2023-02-24 14:40:34 +0700

    create api for register

--- client/src/RegisterAndLoginForm.jsx
+++ client/src/RegisterAndLoginForm.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const RegisterAndLoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className='bg-blue-50 h-screen flex items-center'>
         <form className='w-64 mx-auto mb-12'>
-            <input type="text" placeholder='Username' className='block w-full rounded-sm p-2 mb-2 border'/>
-            <input type="password" placeholder='password' className='block w-full rounded-sm p-2 mb-2 border'/>
+            <input type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder='Username' className='block w-full rounded-sm p-2 mb-2 border'/>
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder='password' className='block w-full rounded-sm p-2 mb-2 border'/>
             <button className='bg-blue-500 text-white block w-full rounded-sm p-2'>
                 Register
             </button>
---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const cookieParser = require('cookie-parser');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("./models/User");
+
+dotenv.config();
+const jwtSecret = process.env.MY_SECRETKEY;
+const bcryptSalt = bcrypt.genSaltSync(10);
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.get("/test", (req, res) => {
+    res.json("test ok")
+});
+
+app.post("/register", async (req, res) => {
+    const {username, password} = req.body;
+    try {
+        const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+        const createdUser = await User.create({
+            username: username,
+            password: hashedPassword
+        });
+        jwt.sign({userId: createdUser._id, username}, jwtSecret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token, {sameSite:'none', secure:true}).status(201).json({
+              id: createdUser._id,
+            });
+        })
+    } catch (error) {
+        if (error) throw error;
+        res.status(500).json('error');
+    }
+
+
+})
+
+const PORT = 4000
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log('Connected to DB');
+    app.listen(PORT, () => {
+        console.log(`Server is running at ${PORT}`);
+    })
+}).catch(err=> {
+    console.log('Error: ', err)
+});
